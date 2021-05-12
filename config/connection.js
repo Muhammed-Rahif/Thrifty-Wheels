e38bc650847faf494271ed893fa5e51ba810f55c
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-05-12 21:32:11 +0530

    mongodb connection file updated for connecting to mongodb atlas

--- config/connection.js
+++ config/connection.js
@@ -4,17 +4,21 @@ const state = {
 }
 
 module.exports.connect = (done) => {
-    const url = 'mongodb://localhost:27017'
+    const url = process.env.MONGODB_URI || "mongodb://localhost:27017";
     const dbname = 'ThriftyWheels'
 
-    mongoClient.connect(url, (err, data) => {
+    mongoClient.connect(
+      url,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      (err, data) => {
         if (err) {
-            return done(err)
+          return done(err);
         } else {
-            state.db = data.db(dbname)
-            done()
+          state.db = data.db(dbname);
+          done();
         }
-    })
+      }
+    );
 }
 module.exports.get = () => {
     return state.db
